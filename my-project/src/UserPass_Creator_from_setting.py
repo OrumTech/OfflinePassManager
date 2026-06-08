d288456d26b3ac5ec5948fee54c3eb0e3e98891e
OrumTech <orumaitech@gmail.com>
2026-06-08 22:55:47 +0330

    i have hash list and master pass chunking for map to hash

--- my-project/src/UserPass_Creator_from_setting.py
+++ my-project/src/UserPass_Creator_from_setting.py
@@ -14,17 +14,23 @@ print(list_of_encrypted)
 # باید وقتی setting انجام شد و دیکشنری رو ساخت خودش رو از بین ببره
 # بعد از ساخت encrypt dic, chunk pass, map to chunk to dic, user passes caller
 
-user_input = [int(d) for d in input("pass: ")]
+user_input = [str(d) for d in input("pass: ")]
+# تا اینجا درسته خط 14 pass میشه. بعدش اینا باید حذف بشند.
 item_counter = 0
 chunk_num = []
 
 for code in lol:
     item_counter = 0
-    for num in code:
+    for num in str(code):
         item_counter += 1
     chunk_num.append(item_counter)
 
+print(chunk_num)
+user_input_to_hash = []
+
 for num in chunk_num:
-    chunk = user_input[:num]
+    user_input_to_hash.append(user_input[:num])
     del user_input[:num]
+
+print(user_input_to_hash)
     
